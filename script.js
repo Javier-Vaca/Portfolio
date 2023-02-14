68a374a834ada65fa97cfe5d2290d8f6264b3d29
Javier <javiervaca2003@gmail.com>
2023-02-14 10:25:02 -0800

    Add toggle function to website and updated the formating of the website

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-let project1 = document.getElementById('project-1');
-let project1Desc = document.getElementById('project-1-desc');
 
-project1Desc.hidden = true;
+// Clicking on a project will show the project description below it (hopefully)
 
-project1.onclick = function() {
-    project1Desc.hidden = false;
-}
+var x = document.getElementById("project-1-desc");
+var y = document.getElementById("project-2-desc");
+var z = document.getElementById("project-3-desc");
+
+x.style.display = 'none';
+y.style.display = 'none';
+z.style.display = 'none';
+
+function toggle() {
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+};
+
+function toggle1() {
+    if (y.style.display === 'none') {
+        y.style.display = 'block';
+    } else {
+        y.style.display = 'none';
+    }
+};
+
+function toggle2() {
+    if (z.style.display === 'none') {
+        z.style.display = 'block';
+    } else {
+        z.style.display = 'none';
+    }
+};
+
+// End of Project toggling
